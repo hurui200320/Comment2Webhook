e65ba1fc0a946073efe0b14aa16792d28ebb5ac8
天空Blond <hurui200320@skyblond.info>
2023-03-31 19:08:40 +0800

    It should work

---
+++ Plugin.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * 评论时触发Webhook。
+ * 
+ * @package Comment2Webhook
+ * @author 天空Blond
+ * @version 1.0.0
+ * @link https://skyblond.info
+ */
+class Comment2Webhook_Plugin implements Typecho_Plugin_Interface
+{
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     * 
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function activate()
+    {
+        Typecho_Plugin::factory('Widget_Feedback')->finishComment = array('Comment2Webhook_Plugin', 'handleComment');
+        Typecho_Plugin::factory('Widget_Comments_Edit')->finishComment = array('Comment2Webhook_Plugin', 'handleComment');
+        Typecho_Plugin::factory('Widget_Service')->sendWebhook = array('Comment2Webhook_Plugin', 'doAsync');
+        
+        return _t('请配置此插件的 webhook_url 以使您的评论推送生效');
+    }
+    
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     * 
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate(){}
+    
+    /**
+     * 获取插件配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+        $webhook_url = new Typecho_Widget_Helper_Form_Element_Text('webhook_url', NULL, NULL, _t('webhook_url'), _t('webhook_url 是插件在收到评论时发送POST请求的url'));
+        $form->addInput($webhook_url->addRule('required', _t('webhook_url 不能为空')));
+    }
+    
+    /**
+     * 个人用户的配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+
+	public static function copyComment(&$result, $comment, $prefix) {
+		$result[''.$prefix.'cid'] = $comment->cid;
+		$result[''.$prefix.'coid'] = $comment->coid;
+		$result[''.$prefix.'created'] = $comment->created;
+		$result[''.$prefix.'author'] = $comment->author;
+		$result[''.$prefix.'authorId'] = $comment->authorId;
+		$result[''.$prefix.'ownerId'] = $comment->ownerId;
+		$result[''.$prefix.'mail'] = $comment->mail;
+		$result[''.$prefix.'ip'] = $comment->ip;
+		$result[''.$prefix.'title'] = $comment->title;
+		$result[''.$prefix.'text'] = $comment->text;
+		$result[''.$prefix.'permalink'] = $comment->permalink;
+		$result[''.$prefix.'status'] = $comment->status;
+		$result[''.$prefix.'parent'] = $comment->parent;
+	}
+	
+	// copied from https://github.com/AlanDecode/Typecho-Plugin-Mailer/blob/0e13e004bb70c4d0e7decc5372e9626c97e2acb2/Plugin.php#L300
+	public static function widgetById($table, $pkId)
+    {
+        $table = ucfirst($table);
+        if (!in_array($table, array('Contents', 'Comments', 'Metas', 'Users'))) {
+            return NULL;
+        }
+
+        $keys = array(
+            'Contents'  =>  'cid',
+            'Comments'  =>  'coid',
+            'Metas'     =>  'mid',
+            'Users'     =>  'uid'
+        );
+
+        $className = "Widget_Abstract_{$table}";
+        $key = $keys[$table];
+        $db = Typecho_Db::get();
+        $widget = $className::alloc();
+        
+        $db->fetchRow($widget->select()->where("{$key} = ?", $pkId)->limit(1),
+                array($widget, 'push'));
+
+        return $widget;
+    }
+
+    /**
+     * 处理评论
+     * 
+     * @access public
+     * @param array $comment 评论结构
+     * @return void
+     */
+    public static function handleComment($comment)
+    {
+		// copy the comment
+		$result = array();
+        self::copyComment($result, $comment, "current_");
+        if(isset($comment->parent)) {
+            $p = self::widgetById('comments', $comment->parent);
+            $parentComment = self::copyComment($result, $p, "parent_");
+        }
+
+        Helper::requestService('sendWebhook', $result);
+    }
+	
+    public static function doAsync($postdata)
+    {
+        $options = Typecho_Widget::widget('Widget_Options');
+        $webhook_url = $options->plugin('Comment2Webhook')->webhook_url;
+
+        $opts = array('http' =>
+            array(
+                'method'  => 'POST',
+                'header'  => 'Content-type: application/x-www-form-urlencoded',
+                'content' => http_build_query($postdata)
+                )
+            );
+        $context = stream_context_create($opts);
+        $result = file_get_contents($webhook_url, false, $context);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Comment2Webhook
 Typecho plugin comment to webhook
+
+苦于找不到合适的评论通知邮件，尤其是发邮件的插件，于是自己写了这个插件，在获得新评论时将评论的相关信息直接POST到Webhook，由其他程序进行后续处理。
+
+## 请求
+
+插件配置中有个`webhook_url`，插件会以`application/x-www-form-urlencoded`的格式向该URL发送POST请求。请求是异步的，因此返回结果会被忽略，虽然是异步的，但仍然建议快速返回。
+
+> 我没写过PHP，我不知道这种异步调用在超时之后会发生什么。我的做法是在第三方程序收到请求后，立刻解析数据，解析成功后加入队列，随后立刻响应请求。然后再用别的线程慢慢处理队列。
+
+请求的参数和`Widget_Feedback`的`finishComment`参数差不多，主要区别在于：
+
++ 当前评论会以`current_`作为前缀，例如当前评论的内容是`current_text`
++ 若有，回复的评论以`parent_`作为前缀，例如被回复的评论内容是`parent_text`
+
+```php
+	public static function copyComment(&$result, $comment, $prefix) {
+		$result[''.$prefix.'cid'] = $comment->cid;
+		$result[''.$prefix.'coid'] = $comment->coid;
+		$result[''.$prefix.'created'] = $comment->created;
+		$result[''.$prefix.'author'] = $comment->author;
+		$result[''.$prefix.'authorId'] = $comment->authorId;
+		$result[''.$prefix.'ownerId'] = $comment->ownerId;
+		$result[''.$prefix.'mail'] = $comment->mail;
+		$result[''.$prefix.'ip'] = $comment->ip;
+		$result[''.$prefix.'title'] = $comment->title;
+		$result[''.$prefix.'text'] = $comment->text;
+		$result[''.$prefix.'permalink'] = $comment->permalink;
+		$result[''.$prefix.'status'] = $comment->status;
+		$result[''.$prefix.'parent'] = $comment->parent;
+	}
+```
